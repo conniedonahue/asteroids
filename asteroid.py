143f547d112597041d26ac5445421a19209e2c38
conniedonahue <connordonahue09@gmail.com>
2025-02-15 16:44:25 -0500

    Add split method

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
+import random
 import pygame
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        neg_vector = self.velocity.rotate(-random_angle)
+        pos_vector = self.velocity.rotate(random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        neg_Asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        neg_Asteroid.velocity = neg_vector * 1.2
+        pos_Asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        pos_Asteroid.velocity = pos_vector * 1.2
+
--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
 
             for shot in shots:
                 if shot.collides_with(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         for obj in drawable:
